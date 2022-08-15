74af6da0f39303dcb5e80375812b9417cba1ceec
Angel Arrieta <anuidev@MacBook-Pro-de-Angel.local>
2022-08-14 22:17:53 -0500

    :sparkles: update

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,12 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-  /*   meta: [
+   meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
-    ], */
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
